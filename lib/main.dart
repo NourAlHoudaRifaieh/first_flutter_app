d5e15bfc371e6dd370a66ff0b3ef907f92fec14b
NourAlHoudaRifaieh <nouralhoudarifaieh@gmail.com>
2026-04-10 03:48:02 +0300

    create first app

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,17 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Home Page'),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: <Widget>[
-            SizedBox(height:16.0,),
-            MyTextWidget(text: 'text 1'),
-            SizedBox(height:16.0,),
-            MyTextWidget(text: 'text 2'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                MyTextWidget(text: 'text 3'),
-                MyTextWidget(text: 'text 4'),
-                MyTextWidget(text:'text 5')
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-class MyTextWidget extends StatelessWidget {
-  final String text;
-
-  const MyTextWidget({super.key, required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        text,
-        style: const TextStyle(
-          fontSize: 24,
-          color: Colors.deepPurple,
-        ),
-      ),
+  Widget build (BuildContext context){
+    return const MaterialApp(
+      title:'CSCI410 week 4 project',
+      debugShowCheckedModeBanner: false,
+      home:Home(),
     );
   }
 }
